c35a1fe571ba2e30f7b696154648f5054e4eb139
mamunurRoshid14 <104456435+mamunurRoshid14@users.noreply.github.com>
2024-06-16 11:41:47 +0600

    Committing files

---
+++ 1917A | Least Product/238679122.cpp
@@ -0,0 +1,50 @@
+//https://codeforces.com/profile/mamunur_roshid_517
+        #include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define pb push_back
+#define ff first
+#define ss second
+#define CC(x) cout << "Case " << ++x << ":";
+#define LL_INF (1LL << 62)
+#define INF (1 << 30)
+#define SetBit(x, k) (x = (1LL << k))
+#define ClearBit(x, k) (x &= ~(1LL << k))
+#define CheckBit(x, k) ((x>>k)&1)
+#define mod 998244353
+#define N 200006
+ 
+void solve(){
+    //First, confirm your idea with pen and paper.
+    bool f=0;
+    ll cn=0;
+    ll n,i,j; cin>>n;
+    ll aa[n];
+    for(i=0;i<n;i++){
+        cin>>aa[i];
+        if(aa[i]==0)f=1;
+        else if(aa[i]<0)cn++;
+    }
+    if(f||cn%2==1){
+        cout<<"0\n";
+    }
+    else{
+        cout<<"1\n1 0\n";
+    }
+    return;
+}
+ 
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t=1;
+    cin >> t;
+    for(int it=1;it<=t;it++){
+        solve();
+    }
+    return 0;
+}
+                //Link: https://codeforces.com/contest/1917/problem/A
