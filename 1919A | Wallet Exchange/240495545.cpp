d56169a408d5361ffb0dde23bba3df171920b8b9
mamunurRoshid14 <104456435+mamunurRoshid14@users.noreply.github.com>
2024-06-16 11:38:09 +0600

    Committing files

---
+++ 1919A | Wallet Exchange/240495545.cpp
@@ -0,0 +1,40 @@
+//https://codeforces.com/profile/mamunur_roshid_517
+        #include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define pb push_back
+#define ff first
+#define ss second
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define CC(x) cout << "Case " << ++x << ":";
+#define LL_INF (1LL << 62)
+#define INF (1 << 30)
+#define SetBit(x, k) (x = (1LL << k))
+#define ClearBit(x, k) (x &= ~(1LL << k))
+#define CheckBit(x, k) ((x>>k)&1)
+#define mod 998244353
+#define N 1000006
+ 
+void solve(){
+    ll a,b; cin>>a>>b;
+    a+=b;
+    if(a%2)cout<<"Alice\n";
+    else cout<<"Bob\n";
+    return;
+}
+ 
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t=1;
+    cin >> t;
+    for(int it=1;it<=t;it++){
+        solve();
+    }
+    return 0;
+}
+                //Link: https://codeforces.com/contest/1919/problem/A
