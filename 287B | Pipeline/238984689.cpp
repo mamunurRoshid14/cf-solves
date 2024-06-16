05b8cc20ee887980f1e992f54029456bf0cf5865
mamunurRoshid14 <104456435+mamunurRoshid14@users.noreply.github.com>
2024-06-16 11:40:43 +0600

    Committing files

---
+++ 287B | Pipeline/238984689.cpp
@@ -0,0 +1,53 @@
+//https://codeforces.com/profile/mamunur_roshid_517
+        #include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define pb push_back
+#define ff first
+#define ss second
+#define CC(x) cout << "Case " << ++x << ":";
+#define LL_INF (1LL << 62)
+#define INF (1 << 30)
+#define SetBit(x, k) (x = (1LL << k))
+#define ClearBit(x, k) (x &= ~(1LL << k))
+#define CheckBit(x, k) ((x>>k)&1)
+#define mod 998244353
+#define N 200006
+ 
+void solve(){
+    //First, confirm your idea with pen and paper.
+    ll n,k;
+    cin>>n>>k;
+    ll l=0,r=k-1;
+    ll an=-1;
+    while(l<=r){
+        ll m=(l+r)/2;
+        ll tm=k-m;
+        ll s1=(k*(k+1))/2;
+        ll s2=(tm*(tm+1))/2;
+        if(s1-s2-m+1>=n){
+            an=m;
+            r=m-1;
+        }
+        else{
+            l=m+1;
+        }
+    }
+    cout<<an<<"\n";
+    return;
+}
+ 
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t=1;
+   // cin >> t;
+    for(int it=1;it<=t;it++){
+        solve();
+    }
+    return 0;
+}
+                //Link: https://codeforces.com/contest/287/problem/B
