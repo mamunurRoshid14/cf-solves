9f03a04e094e2b6f358a9b8206401f433821bff3
mamunurRoshid14 <104456435+mamunurRoshid14@users.noreply.github.com>
2024-06-16 11:28:11 +0600

    Committing files

---
+++ 1951A | Dual Trigger/255286570.cpp
@@ -0,0 +1,51 @@
+//https://codeforces.com/profile/mamunur_roshid_517
+        #include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pll pair<long long, long long>
+#define pb push_back
+#define ff first
+#define ss second
+#define LL_INF (1LL << 62)
+#define SetBit(x, k) (x = (1LL << k))
+#define ClearBit(x, k) (x &= ~(1LL << k))
+#define CheckBit(x, k) ((x>>k)&1)
+#define mod 998244353
+void solve(){
+    //First, confirm your idea with pen and paper.
+    ll n,i; cin>>n;
+    string aa; cin>>aa;
+    ll cn=0;
+    for(i=0;i<n;i++){
+        if(aa[i]=='1')cn++;
+    }
+    if(cn==2){
+        for(i=1;i<n;i++){
+            if(aa[i]==aa[i-1]&&aa[i]=='1'){
+                cout<<"NO\n";
+                return;
+            }
+        }
+    }
+    if(n==2&&cn){
+        cout<<"NO\n";
+    }
+    else if(cn%2==0){
+        cout<<"YES\n";
+    }
+    else{
+        cout<<"NO\n";
+    }
+    return;
+}
+int32_t main(){
+    ios::sync_with_stdio(false); cin.tie(0);
+    int t=1;
+    cin >> t;
+    for(int it=1;it<=t;it++){
+        //cout<<"Case "<<it<<": ";
+        solve();
+    }
+    return 0;
+}
+                //Link: https://codeforces.com/contest/1951/problem/A
