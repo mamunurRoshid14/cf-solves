04e82deed0eeb31a76c9a297c922eec74aed7841
mamunurRoshid14 <104456435+mamunurRoshid14@users.noreply.github.com>
2024-06-16 11:30:28 +0600

    Committing files

---
+++ 1945A | Setting up Camp/252194706.cpp
@@ -0,0 +1,37 @@
+//https://codeforces.com/profile/mamunur_roshid_517
+        #include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pll pair<long long, long long>
+#define pb push_back
+#define ff first
+#define ss second
+#define LL_INF (1LL << 62)
+#define SetBit(x, k) (x = (1LL << k))
+#define ClearBit(x, k) (x &= ~(1LL << k))
+#define CheckBit(x, k) ((x>>k)&1)
+#define mod 998244353
+void solve(){
+    //First, confirm your idea with pen and paper.
+    ll a,b,c; cin>>a>>b>>c;
+    if((b%3)+c<3&&b%3){
+        cout<<"-1\n";
+        return;
+    }
+    a+=(b+c)/3;
+    if((b+c)%3)a++;
+    cout<<a<<"\n";
+ 
+    return;
+}
+int32_t main(){
+    ios::sync_with_stdio(false); cin.tie(0);
+    int t=1;
+    cin >> t;
+    for(int it=1;it<=t;it++){
+        //cout<<"Case "<<it<<": ";
+        solve();
+    }
+    return 0;
+}
+                //Link: https://codeforces.com/contest/1945/problem/A
