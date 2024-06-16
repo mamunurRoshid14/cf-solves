bd3dd4671907b8a300b471879114cef955146976
mamunurRoshid14 <104456435+mamunurRoshid14@users.noreply.github.com>
2024-06-16 11:32:58 +0600

    Committing files

---
+++ 1927D | Find the Different Ones!/245124117.cpp
@@ -0,0 +1,55 @@
+//https://codeforces.com/profile/mamunur_roshid_517
+        #include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pll pair<long long, long long>
+#define pb push_back
+#define ff first
+#define ss second
+#define LL_INF (1LL << 62)
+#define SetBit(x, k) (x = (1LL << k))
+#define ClearBit(x, k) (x &= ~(1LL << k))
+#define CheckBit(x, k) ((x>>k)&1)
+#define mod 998244353
+void solve(){
+    //First, confirm your idea with pen and paper.
+    ll n,i,j; cin>>n;
+    ll aa[n+3];
+    for(i=1;i<=n;i++)cin>>aa[i];
+    ll an[n+3];
+    ll inf=n+2;
+    an[n]=inf;
+    for(i=n-1;i>0;i--){
+        if(aa[i]==aa[i+1]){
+            an[i]=an[i+1];
+        }
+        else{
+            an[i]=i+1;
+        }
+    }
+    ll q; cin>>q;
+    while(q--){
+        ll l,r; cin>>l>>r;
+        ll tm=an[l];
+        if(tm<=r){
+            cout<<l<<" "<<tm<<"\n";
+        }
+        else{
+            cout<<"-1 -1\n";
+        }
+ 
+    }
+    cout<<"\n";
+    return;
+}
+int32_t main(){
+    ios::sync_with_stdio(false); cin.tie(0);
+    int t=1;
+    cin >> t;
+    for(int it=1;it<=t;it++){
+        //cout<<"Case "<<it<<": ";
+        solve();
+    }
+    return 0;
+}
+                //Link: https://codeforces.com/contest/1927/problem/D
