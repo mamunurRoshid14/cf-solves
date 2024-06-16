7cb90cf0bb3fd4c162efeb8f41e8090a170dbb12
mamunurRoshid14 <104456435+mamunurRoshid14@users.noreply.github.com>
2024-06-16 11:39:15 +0600

    Committing files

---
+++ 1916B | Two Divisors/239683417.cpp
@@ -0,0 +1,51 @@
+//https://codeforces.com/profile/mamunur_roshid_517
+        #include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define pb push_back
+#define ff first
+#define ss second
+#define CC(x) cout << "Case " << ++x << ":";
+#define LL_INF (1LL << 62)
+#define INF (1 << 30)
+#define SetBit(x, k) (x = (1LL << k))
+#define ClearBit(x, k) (x &= ~(1LL << k))
+#define CheckBit(x, k) ((x>>k)&1)
+#define mod 998244353
+#define N 200006
+ 
+void solve() {
+    //First, confirm your idea with pen and paper.
+    ll a,b,i,j;
+    cin>>a>>b;
+    ll gc=__gcd(a,b);
+    if(a==1){
+        cout<<b*b<<"\n";
+        return;
+    }
+    if(gc==a){
+        cout<<b*(b/gc)<<"\n";
+    }
+    else{
+        a/=gc;
+        b/=gc;
+        cout<<a*b*gc<<"\n";
+    }
+    return;
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t=1;
+    cin >> t;
+    for(int it=1; it<=t; it++) {
+        solve();
+    }
+    return 0;
+}
+ 
+                //Link: https://codeforces.com/contest/1916/problem/B
