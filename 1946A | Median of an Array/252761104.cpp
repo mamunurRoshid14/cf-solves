0a63c5108df09f111c4e29c0a744ff90dddf6240
mamunurRoshid14 <104456435+mamunurRoshid14@users.noreply.github.com>
2024-06-16 11:29:32 +0600

    Committing files

---
+++ 1946A | Median of an Array/252761104.cpp
@@ -0,0 +1,42 @@
+//https://codeforces.com/profile/mamunur_roshid_517
+        #include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pll pair<long long, long long>
+#define pb push_back
+#define ff first
+#define ss second
+#define LL_INF (1LL << 62)
+#define SetBit(x, k) (x = (1LL << k))
+#define ClearBit(x, k) (x &= ~(1LL << k))
+#define CheckBit(x, k) ((x>>k)&1)
+void solve() {
+    //First, confirm your idea with pen and paper.
+    ll n,i; cin>>n;
+    ll aa[n+3];
+    for(i=0;i<n;i++)cin>>aa[i];
+    sort(aa,aa+n);
+    aa[n]=INT_MAX;
+    ll m=(n+1)/2;
+    ll an=1;
+    for(i=m-1;i<n;i++){
+        if(aa[i]==aa[i+1]){
+            an++;
+        }
+        else break;
+    }
+    cout<<an<<"\n";
+    return;
+}
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t=1;
+    cin >> t;
+    for(int it=1; it<=t; it++) {
+        //cout<<"Case "<<it<<": ";
+        solve();
+    }
+    return 0;
+}
+                //Link: https://codeforces.com/contest/1946/problem/A
