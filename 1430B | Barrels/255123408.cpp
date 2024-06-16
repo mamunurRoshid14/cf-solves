043c25cffebdce63dd22592e386495953acdc5be
mamunurRoshid14 <104456435+mamunurRoshid14@users.noreply.github.com>
2024-06-16 12:24:33 +0600

    Committing files

---
+++ 1430B | Barrels/255123408.cpp
@@ -0,0 +1,36 @@
+//https://codeforces.com/profile/mamunur_roshid_517
+        #include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pll pair<long long, long long>
+#define pb push_back
+#define ff first
+#define ss second
+#define LL_INF (1LL << 62)
+#define SetBit(x, k) (x = (1LL << k))
+#define ClearBit(x, k) (x &= ~(1LL << k))
+#define CheckBit(x, k) ((x>>k)&1)
+#define mod 998244353
+void solve(){
+    //First, confirm your idea with pen and paper.
+    ll n,i,j,k; cin>>n>>k;
+    ll aa[n]; for(i=0;i<n;i++)cin>>aa[i];
+    sort(aa,aa+n);
+    ll s=aa[n-1];
+    for(i=max(0LL,n-k-1);i<n-1;i++){
+        s+=aa[i];
+    }
+    cout<<s<<"\n";
+    return;
+}
+int32_t main(){
+    ios::sync_with_stdio(false); cin.tie(0);
+    int t=1;
+    cin >> t;
+    for(int it=1;it<=t;it++){
+        //cout<<"Case "<<it<<": ";
+        solve();
+    }
+    return 0;
+}
+                //Link: https://codeforces.com/contest/1430/problem/B
