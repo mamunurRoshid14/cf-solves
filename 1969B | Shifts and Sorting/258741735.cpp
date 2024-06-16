529b1fc6a2ea92e6090f5a4e2305698537b9a563
mamunurRoshid14 <104456435+mamunurRoshid14@users.noreply.github.com>
2024-06-16 11:26:53 +0600

    Committing files

---
+++ 1969B | Shifts and Sorting/258741735.cpp
@@ -0,0 +1,39 @@
+//https://codeforces.com/profile/mamunur_roshid_517
+        #include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pll pair<long long, long long>
+#define pb push_back
+#define ff first
+#define ss second
+#define LL_INF (1LL << 62)
+#define SetBit(x, k) (x = (1LL << k))
+#define ClearBit(x, k) (x &= ~(1LL << k))
+#define CheckBit(x, k) ((x>>k)&1)
+void solve() {
+    //First, confirm your idea with pen and paper.
+    string  aa; cin>>aa;
+    ll n=aa.size(),i,j;
+    ll an=0,cn=0;
+    for(i=0;i<n;i++){
+        if(aa[i]=='1')cn++;
+        else{
+            if(cn)
+            an+=(cn+1);
+        }
+    }
+    cout<<an<<"\n";
+    return;
+}
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t=1;
+    cin >> t;
+    for(int it=1; it<=t; it++) {
+        //cout<<"Case "<<it<<": ";
+        solve();
+    }
+    return 0;
+}
+                //Link: https://codeforces.com/contest/1969/problem/B
