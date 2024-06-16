7f5373f87048c4dd57106125a121cf3d54291a99
mamunurRoshid14 <104456435+mamunurRoshid14@users.noreply.github.com>
2024-06-16 11:38:06 +0600

    Committing files

---
+++ 1919B | Plus-Minus Split/240505494.cpp
@@ -0,0 +1,47 @@
+//https://codeforces.com/profile/mamunur_roshid_517
+        #include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define pb push_back
+#define ff first
+#define ss second
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define CC(x) cout << "Case " << ++x << ":";
+#define nn "\n"
+#define LL_INF (1LL << 62)
+#define INF (1 << 30)
+#define SetBit(x, k) (x = (1LL << k))
+#define ClearBit(x, k) (x &= ~(1LL << k))
+#define CheckBit(x, k) ((x>>k)&1)
+#define mod 998244353
+#define N 1000006
+ 
+void solve(){
+    ll n; cin>>n;
+    string aa; cin>>aa;
+    ll cn1=0,cn2=0;
+    for(auto ii:aa){
+        if(ii=='+')cn1++;
+        else cn2++;
+    }
+    ll mn=min(cn1,cn2);
+    if(cn1<cn2)swap(cn1,cn2);
+    cout<<cn1-mn<<"\n";
+    return;
+}
+ 
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t=1;
+    cin >> t;
+    for(int it=1;it<=t;it++){
+        solve();
+    }
+    return 0;
+}
+                //Link: https://codeforces.com/contest/1919/problem/B
