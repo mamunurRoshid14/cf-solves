73e4e85634917d7e057758aea06c946249a142a9
mamunurRoshid14 <104456435+mamunurRoshid14@users.noreply.github.com>
2024-06-16 11:45:39 +0600

    Committing files

---
+++ 1907E | Good Triples/236255513.cpp
@@ -0,0 +1,51 @@
+//https://codeforces.com/profile/mamunur_roshid_517
+        #include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define pb push_back
+#define ff first
+#define ss second
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define CC(x) cout << "Case " << ++x << ":";
+#define nn "\n"
+#define LL_INF (1LL << 62)
+#define INF (1 << 30)
+#define SetBit(x, k) (x = (1LL << k))
+#define ClearBit(x, k) (x &= ~(1LL << k))
+#define CheckBit(x, k) ((x>>k)&1)
+#define mod 998244353
+#define N 1000006
+ll f(ll n){
+    ll an=1;
+    for(ll i=n+1;i<=n+2;i++){
+        an*=i;
+    }
+    an/=2;
+    return an;
+}
+void solve(){
+    ll n; cin>>n;
+    ll an=1;
+    while(n){
+        ll d=n%10; n/=10;
+        an*=f(d);
+    }
+    cout<<an<<"\n";
+    return;
+}
+ 
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t=1;
+    cin >> t;
+    for(int it=1;it<=t;it++){
+        solve();
+    }
+    return 0;
+}
+                //Link: https://codeforces.com/contest/1907/problem/E
