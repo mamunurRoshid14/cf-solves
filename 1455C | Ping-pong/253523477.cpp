ddf59a9f8e0ceaa8efc58b7a49124d9aeefd51cf
mamunurRoshid14 <104456435+mamunurRoshid14@users.noreply.github.com>
2024-06-16 11:29:06 +0600

    Committing files

---
+++ 1455C | Ping-pong/253523477.cpp
@@ -0,0 +1,30 @@
+//https://codeforces.com/profile/mamunur_roshid_517
+        #include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pll pair<long long, long long>
+#define pb push_back
+#define ff first
+#define ss second
+#define LL_INF (1LL << 62)
+#define SetBit(x, k) (x = (1LL << k))
+#define ClearBit(x, k) (x &= ~(1LL << k))
+#define CheckBit(x, k) ((x>>k)&1)
+#define mod 998244353
+void solve(){
+    //First, confirm your idea with pen and paper.
+    ll a,b,i,j; cin>>a>>b;
+    cout<<a-1<<" "<<b<<"\n";
+    return;
+}
+int32_t main(){
+    ios::sync_with_stdio(false); cin.tie(0);
+    int t=1;
+    cin >> t;
+    for(int it=1;it<=t;it++){
+        //cout<<"Case "<<it<<": ";
+        solve();
+    }
+    return 0;
+}
+                //Link: https://codeforces.com/contest/1455/problem/C
