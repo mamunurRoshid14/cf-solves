90e66c28f75bd8f491e2f7f20d090fe1874bd54d
mamunurRoshid14 <104456435+mamunurRoshid14@users.noreply.github.com>
2024-06-16 11:35:00 +0600

    Committing files

---
+++ 1380C | Create The Teams/242722435.cpp
@@ -0,0 +1,56 @@
+//https://codeforces.com/profile/mamunur_roshid_517
+        #include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define pb push_back
+#define ff first
+#define ss second
+#define CC(x) cout << "Case " << ++x << ":";
+#define LL_INF (1LL << 62)
+#define INF (1 << 30)
+#define SetBit(x, k) (x = (1LL << k))
+#define ClearBit(x, k) (x &= ~(1LL << k))
+#define CheckBit(x, k) ((x>>k)&1)
+#define mod 998244353
+#define N 200006
+ 
+ll add(ll b, ll c){
+    if(LL_INF-b<c) return LL_INF+1;
+    return b+c;
+}
+ll mul(ll b, ll c){
+    if(LL_INF/b<c) return LL_INF+1;
+    return b*c;
+}
+void solve(){
+    //First, confirm your idea with pen and paper.
+    ll n,x,i,j; cin>>n>>x;
+    ll aa[n]; for(i=0;i<n;i++)cin>>aa[i];
+    sort(aa,aa+n);
+    ll an=0;
+    ll cn=0;
+    for(i=n-1;i>=0;i--){
+        cn++;
+        if(cn*aa[i]>=x){
+            an++;
+            cn=0;
+        }
+    }
+    cout<<an<<"\n";
+    return;
+}
+ 
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t=1;
+    cin >> t;
+    for(int it=1;it<=t;it++){
+        solve();
+    }
+    return 0;
+}
+                //Link: https://codeforces.com/contest/1380/problem/C
