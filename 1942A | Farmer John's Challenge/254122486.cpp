8a63536f657db40f259f1f580c82e6196215a21e
mamunurRoshid14 <104456435+mamunurRoshid14@users.noreply.github.com>
2024-06-16 11:29:03 +0600

    Committing files

---
+++ 1942A | Farmer John's Challenge/254122486.cpp
@@ -0,0 +1,44 @@
+//https://codeforces.com/profile/mamunur_roshid_517
+        #include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pll pair<long long, long long>
+#define pb push_back
+#define ff first
+#define ss second
+#define LL_INF (1LL << 62)
+#define SetBit(x, k) (x = (1LL << k))
+#define ClearBit(x, k) (x &= ~(1LL << k))
+#define CheckBit(x, k) ((x>>k)&1)
+#define mod 998244353
+void solve(){
+    //First, confirm your idea with pen and paper.
+    ll n,k,i; cin>>n>>k;
+    if(n==k){
+        for(i=0;i<n;i++){
+            cout<<"1 ";
+        }
+        cout<<"\n";
+    }
+    else if(k==1){
+        for(i=0;i<n-1;i++){
+            cout<<"1 ";
+        }
+        cout<<100000<<"\n";
+    }
+    else{
+        cout<<"-1\n";
+    }
+    return;
+}
+int32_t main(){
+    ios::sync_with_stdio(false); cin.tie(0);
+    int t=1;
+    cin >> t;
+    for(int it=1;it<=t;it++){
+        //cout<<"Case "<<it<<": ";
+        solve();
+    }
+    return 0;
+}
+                //Link: https://codeforces.com/contest/1942/problem/A
