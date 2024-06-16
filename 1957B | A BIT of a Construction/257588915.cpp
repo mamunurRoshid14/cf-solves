6482b0bbad9407f7fd8a5aff0472b211859984e9
mamunurRoshid14 <104456435+mamunurRoshid14@users.noreply.github.com>
2024-06-16 11:27:11 +0600

    Committing files

---
+++ 1957B | A BIT of a Construction/257588915.cpp
@@ -0,0 +1,46 @@
+//https://codeforces.com/profile/mamunur_roshid_517
+        #include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pll pair<long long, long long>
+#define pb push_back
+#define ff first
+#define ss second
+#define LL_INF (1LL << 62)
+#define SetBit(x, k) (x |= (1LL << k))
+#define ClearBit(x, k) (x &= ~(1LL << k))
+#define CheckBit(x, k) ((x>>k)&1)
+void solve() {
+    //First, confirm your idea with pen and paper.
+    ll n,k; cin>>n>>k;
+    ll num=0;
+    ll cur=1;
+    ll kk=k;
+    while(k>=cur){
+        num|=cur;
+        k-=cur;
+        cur*=2LL;
+    }
+    if(n==1){
+        cout<<kk<<"\n";
+        return;
+    }
+    cout<<num<<" "<<k<<" ";
+    for(ll i=3;i<=n;i++){
+        cout<<"0 ";
+    }
+    cout<<"\n";
+    return;
+}
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t=1;
+    cin >> t;
+    for(int it=1; it<=t; it++) {
+        //cout<<"Case "<<it<<": ";
+        solve();
+    }
+    return 0;
+}
+                //Link: https://codeforces.com/contest/1957/problem/B
