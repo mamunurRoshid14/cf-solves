62fa444507c73bea649785ee53a5d3ceecff9712
mamunurRoshid14 <104456435+mamunurRoshid14@users.noreply.github.com>
2024-06-16 11:57:29 +0600

    Committing files

---
+++ 1872A | Two Vessels/222220415.cpp
@@ -0,0 +1,26 @@
+//https://codeforces.com/profile/mamunur_roshid_517
+        #include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define F first
+#define S second
+#define mod 1000000007
+void solve(){
+    double a,b,c; cin>>a>>b>>c;
+    a=abs(a-b);
+    a/=2.0;
+    cout<<ceil(a/c)<<"\n";
+}
+int main(){
+    //ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    ll t=1;
+    cin>>t;
+    for(ll it=1; it<=t; it++)
+    {
+        //cout<<"Case "<<it<<": "; //cout<<'\n';
+        solve();
+    }
+    return 0;
+}
+ 
+                //Link: https://codeforces.com/contest/1872/problem/A
